222b7a48ce9e5828c00cc6d4082c8a7eaa875ef9
Haydz6 <cookiegamespa@gmail.com>
2022-09-02 15:27:59 +1000

    Studio support

--- main.go
+++ main.go
@@ -5,24 +5,49 @@ import (
 	"time"
 )
 
-func main() {
+func UpdateClient(){
 	VersionDirectory := GetVersionDirectory()
+	ContentDirectory := GetContentsDirectory()
 
 	if IsAlreadyUpdated(VersionDirectory) {
 		println("Client already updated")
-		time.Sleep(time.Second * 3)
 		return
 	}
 
-	Success1 := WriteFile("https://haydz.cf/files/oldrobloxfiles/coloredlogo.png", path.Join(GetContentsDirectory(), "textures", "ui", "TopBar", "coloredlogo.png"))
-	Success2 := WriteFile("https://haydz.cf/files/oldrobloxfiles/ouch.ogg", path.Join(GetContentsDirectory(), "sounds", "ouch.ogg"))
-	Success3 := WriteFile("https://haydz.cf/files/oldrobloxfiles/robloxTilt.png", path.Join(GetContentsDirectory(), "textures", "loading", "robloxTilt.png"))
+	Success1 := WriteFile("https://haydz.cf/files/oldrobloxfiles/coloredlogo.png", path.Join(ContentDirectory, "textures", "ui", "TopBar", "coloredlogo.png"))
+	Success2 := WriteFile("https://haydz.cf/files/oldrobloxfiles/ouch.ogg", path.Join(ContentDirectory, "sounds", "ouch.ogg"))
+	Success3 := WriteFile("https://haydz.cf/files/oldrobloxfiles/robloxTilt.png", path.Join(ContentDirectory, "textures", "loading", "robloxTilt.png"))
 
 	if Success1 && Success2 && Success3 {
 		TagAsUpdated(VersionDirectory)
 	} else {
 		println("\nOne or more files failed to update!")
 	}
+}
+
+func UpdateStudio(){
+	StudioDirectory := GetRobloxStudioDirectory()
+	StudioContentDirectory := GetStudioContentsDirectory()
+
+	if IsAlreadyUpdated(StudioDirectory) {
+		println("Studio already updated")
+		return
+	}
+
+	Success1 := WriteFile("https://haydz.cf/files/oldrobloxfiles/coloredlogo.png", path.Join(StudioContentDirectory, "textures", "ui", "TopBar", "coloredlogo.png"))
+	Success2 := WriteFile("https://haydz.cf/files/oldrobloxfiles/ouch.ogg", path.Join(StudioContentDirectory, "sounds", "ouch.ogg"))
+	Success3 := WriteFile("https://haydz.cf/files/oldrobloxfiles/robloxTilt.png", path.Join(StudioContentDirectory, "textures", "loading", "robloxTilt.png"))
+
+	if Success1 && Success2 && Success3 {
+		TagAsUpdated(StudioDirectory)
+	} else {
+		println("\nOne or more files failed to update!")
+	}
+}
+
+func main() {
+	UpdateClient()
+	UpdateStudio()
 
 	println("\nDone")
 	time.Sleep(time.Second * 3)
